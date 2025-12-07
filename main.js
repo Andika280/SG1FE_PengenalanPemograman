21e03a2a53374f1e254b0935f28f9221d5df77f5
Andika280 <andikaraihan871@gmail,com>
2025-12-07 16:48:41 +0700

    feat : age logic

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-// Memanggil library prompt-sync sesuai materi Conditional.js
 const input = require("prompt-sync")({ sigint: true });
 
 console.log("--- Sistem Security Club Coding ---");
+
+const nama = input("Masukkan Nama Kamu: ");
+const umur = Number(input("Masukkan Umur Kamu: "));
+
+// Logika Umur
+if (umur < 21) {
+    const selisih = 21 - umur;
+    // Menggunakan Template Literal sesuai materi Object.js
+    console.log(`Maaf ${nama}, umur kamu kurang ${selisih} tahun lagi`);
+} else {
+    // Pesan sementara sebelum logika uang dibuat
+    console.log("Umur lolos, lanjut cek uang...");
+}
